94e032ecc10d95146802381fa72ad8a9c256badf
dmzimmerman13 <fondyzim@hotmail.com>
2016-02-23 20:36:51 -0600

    Initial commit

---
+++ app/src/main/java/com/cs407_android/rockpaperscissors/PlayActivity.java
@@ -0,0 +1,58 @@
+package com.cs407_android.rockpaperscissors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        //TA implementation
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_fragment_container, PlayFragment.newInstance(null, null))
+                .addToBackStack(null)
+                .commit();
+        //TA end
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_play, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+
+
+
+        }
+    }
+}
